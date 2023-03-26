4225b667ce518eff8204d19121a2aa22b9d9c33e
afifdgr <afifdgr3@gmail.com>
2023-03-26 13:45:18 +0700

    # Model Controller Route - Blog

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
 
 const app = express();
+
 app.use(express.json());
+
 app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
 
 mongoose
   .connect(
---
+++ controllers/blog-controller.js
@@ -0,0 +1,87 @@
+import Blog from "../model/Blog";
+
+export const getAllBlogs = async (req, res, next) => {
+  let blogs;
+  try {
+    blogs = await Blog.find();
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!blogs) {
+    return res.status(404).json({ message: "No Blogs Found" });
+  }
+  return res.status(200).json({ blogs });
+};
+
+export const addBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+  const blog = new Blog({
+    title,
+    description,
+    image,
+    user,
+  });
+
+  try {
+    await blog.save();
+  } catch (err) {
+    return console.log(err);
+  }
+  return res.status(200).json({ blog });
+};
+
+export const updateBlog = async (req, res, next) => {
+  const { title, description } = req.body;
+  const blogId = req.params.id;
+  let blog;
+
+  try {
+    blog = await Blog.findByIdAndUpdate(blogId, {
+      title,
+      description,
+    });
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!blog) {
+    return res.status(500).json({ message: "Unable to update the blog" });
+  }
+
+  return res.status(200).json({ blog, message: "Success" });
+};
+
+export const getById = async (req, res, next) => {
+  const id = req.params.id;
+  let blog;
+
+  try {
+    blog = await Blog.findById(id);
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!blog) {
+    return res.status(404).json({ message: "No blog found" });
+  }
+
+  return res.status(200).json({ blog });
+};
+
+export const deleteBlog = async (req, res, next) => {
+  const id = req.params.id;
+
+  let blog;
+  try {
+    blog = await Blog.findByIdAndRemove(id);
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!blog) {
+    return res.status(500).json({ message: "Unable to delete" });
+  }
+
+  return res.status(200).json({ message: "Successfully Delete!" });
+};
